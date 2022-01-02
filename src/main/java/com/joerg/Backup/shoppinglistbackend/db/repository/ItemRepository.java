02bb5a3d81cfb7b7cff712a415c3cbc91387ffc2
Joerg Schmitz <schmitz873@gmail.com>
2022-01-02 15:52:05 +0100

    Deleted backup directory

--- src/main/java/com/joerg/Backup/shoppinglistbackend/db/repository/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.joerg.shoppinglistbackend.db.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.joerg.shoppinglistbackend.model.Item;
-
-public interface ItemRepository extends JpaRepository<Item, Long> {
-
-}
